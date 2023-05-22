8e5bc65c05b1c3321b764daed026611615bb9782
Julio Donofreo <julio.bibico@hotmail.com>
2023-05-22 13:33:15 -0300

    DepartmentForm design

--- src/db/DbException.java
+++ src/db/DbException.java
@@ -1,8 +1,9 @@
 package db;
 
 public class DbException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
 
-    public DbException(String message) {
+	public DbException(String message) {
         super(message);
     }
 }
